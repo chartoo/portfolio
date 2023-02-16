a0f23b29f49dc0777b21e4e5e6f722c72414afd0
akh <dev.aungkyawhtwe@gmail.com>
2023-02-16 20:58:00 +0630

    modify : contents and add profile image

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('bg-dark-sticky');
+        } else {
+            document.getElementById('navbar_top').classList.remove('bg-dark-sticky');
+        } 
+    });
+    }); 
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
